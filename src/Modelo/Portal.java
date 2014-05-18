710364829a59f269cdc78ada73baadfe7a9d7899
jeanpantoja <je.cspantoja@gmail.com>
2014-05-18 19:52:24 -0300

    Criando formulario do portal

---
+++ src/Modelo/Portal.java
@@ -0,0 +1,84 @@
+package Modelo;
+
+import Modelo.*;
+
+import java.util.List;
+
+public class Portal implements Observador, Observado
+{
+    private RepositorioDoJogo repositorioJogo;
+    private Jogador jogador;
+
+    public Portal()
+    {
+        this.repositorioJogo = new RepositorDeJogoEmMemoria();
+    }
+
+    public void setJogador( Jogador j )
+    {
+        this.jogador = j;
+    }
+
+    public void autenticar()
+    {
+        if( !this.repositorioJogo.autenticar( this.jogador ) )
+        {
+            /*
+                Mudar Interface grafica dizendo q nao pode logar
+             */
+            System.out.println("NAO AUTENTICOU");
+        }
+        else
+        {
+            /*
+                Mudar Interface grafica AUTENTICOU
+             */
+            System.out.println("AUTENTICOU");
+        }
+
+    }
+
+    public void criarJogador( Jogador j )
+    {
+        this.repositorioJogo.salvar( j );
+    }
+
+    public List<Carta> obterAcervo()
+    {
+        return this.repositorioJogo.obterCartasDoAcervo();
+    }
+
+    public Jogador obterJogadorLogado()
+    {
+        return this.jogador;
+    }
+
+    public void salvarJogador( Jogador jogador )
+    {
+
+    }
+
+    @Override
+    public void registrar(Observador o)
+    {
+
+    }
+
+    @Override
+    public void remover(Observador o)
+    {
+
+    }
+
+    @Override
+    public void notificarObservadores( Object msg )
+    {
+
+    }
+
+    @Override
+    public void notificar( Observado fonte, Object msg )
+    {
+
+    }
+}
--- src/Principal.java
+++ src/Principal.java
@@ -1,13 +1,28 @@
+
 import Modelo.Portal;
 import Modelo.Jogador;
 
-public class Principal
-{
-    public static void main(String[] args)
-    {
+public class Principal {
+
+    public static void fixarTemaJava(){
+     try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+        
+        }
+    }
+    
+    public static void main(String[] args) {
+        fixarTemaJava();
+        
         Portal p = new Portal();
-        p.criarJogador( new Jogador("LUIZ","12345") );
-        p.setJogador( new Jogador("OUTRO USUARIO","SENHA DELE") );
+        p.criarJogador(new Jogador("LUIZ", "12345"));
+        p.setJogador(new Jogador("OUTRO USUARIO", "SENHA DELE"));
         p.autenticar();
     }
 }
