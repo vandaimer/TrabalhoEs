7862d0b6546c3d56a65b484fa22fea67a170cfbc
jeanpantoja <je.cspantoja@gmail.com>
2014-05-19 16:39:04 -0300

    criando form editor de baralho

--- src/Visao/janelas/FormEditorJogador.java
+++ src/Visao/janelas/FormEditorJogador.java
@@ -31,7 +31,6 @@ public class FormEditorJogador extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-  
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
