4be8ffc54a2758ec8d8df8840ca3e415d2443f48
Cesar <cesarsjb@yahoo.com.br>
2014-05-19 23:16:12 -0300

    alteracao classe baralho e testes

--- src/testes/pombo/Teste.java
+++ src/testes/pombo/Teste.java
@@ -6,12 +6,31 @@
 
 package testes.pombo;
 
+import Modelo.Baralho;
+import Modelo.Carta;
+
 /**
  *
  * @author jean
  */
 public class Teste {
     public static void main(String[] args) {
-        
+    	
+    	Baralho b = new Baralho();
+    	
+    	Carta c1 = new Carta("as de espada");
+    	//Carta c1 = new Carta("3 de paus");
+    	Carta c2 = new Carta("rei de ouro");
+    	Carta c3 = new Carta("valete de copas");
+    	
+    	
+    	b.adicionar(c1);
+    	b.adicionar(c2);
+    	b.adicionar(c1);
+    	b.adicionar(c3);
+    	b.adicionar(c1);
+    	
+    	System.out.println(b.verificaRepeticao());
+    	
     }
 }
