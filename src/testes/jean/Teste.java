0c4076ab5011aa53f3acfc1907c8cb54ba1f90e9
jeanpantoja <je.cspantoja@gmail.com>
2014-07-06 19:36:20 -0300

    zennntiii

--- src/testes/jean/Teste.java
+++ src/testes/jean/Teste.java
@@ -6,19 +6,20 @@ import modelo.Carta;
 import modelo.ControleRemoto;
 import modelo.Jogador;
 import modelo.Portal;
-import modelo.RepositorDeJogoEmMemoria;
+import modelo.RepositorioDeJogoDB;
+import modelo.persistencia.Fabrica;
 
 public class Teste {
-
+    
     public static void main(String[] args) throws IOException, InterruptedException {
-
-        Portal portal = new Portal(new RepositorDeJogoEmMemoria());
-        Jogador j = new Jogador("jean", "1");
-        portal.criarJogador(j);
-        portal.autenticar(j);
+        
+        Portal portal = new Portal(new RepositorioDeJogoDB(new Fabrica("150.162.52.177", "jogoes", "@fuckingpassword@", "engenhariasoftware")));
+        Jogador j = new Jogador("jean", "1234");
+        portal.autenticar(j);        
         ControleRemoto ctr = portal.conectarAoOponente("192.168.1.3", 1234);
+        Thread.sleep(2000);
         ctr.jogar(new LinkedList<Carta>());
-
+        
     }
-
+    
 }
