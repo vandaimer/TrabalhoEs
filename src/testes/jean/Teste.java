41536a61100b1e0577afea79559f586b87f8a664
jeanpantoja <je.cspantoja@gmail.com>
2014-07-06 17:49:33 -0300

    mudancas teste

--- src/testes/jean/Teste.java
+++ src/testes/jean/Teste.java
@@ -1,10 +1,6 @@
 package testes.jean;
 
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-import modelo.ConectorCliente;
 import modelo.ControleRemoto;
 import modelo.Jogador;
 import modelo.Portal;
@@ -19,20 +15,7 @@ public class Teste {
         portal.criarJogador(j);
         portal.autenticar(j);
         ControleRemoto ctr = portal.conectarAoOponente("177.132.96.43", 1234);
-        //ControleRemoto ctr = portal.conectarAoOponente("192.168.1.3", 1234);
-        conectar("177.132.96.43", 1234);
-    }
-
-    public static void conectar(String ip, int porta) {
-        try {
-
-            Socket sc = new Socket(ip, porta);
-            ConectorCliente con = new ConectorCliente(sc);
-            System.out.println(con.receber());
-            con.enviar("porraaaa");
-            sc.close();
-        } catch (Exception e) {
-        }
+       
     }
 
 }
