36a0c301697c3e212aab36393ff5b4daf0c6d005
rodrigoPOO <rodrigop_mello@hotmail.com>
2014-07-06 21:20:38 -0300

    tibia viado

--- src/modelo/AguardandoJogada.java
+++ src/modelo/AguardandoJogada.java
@@ -5,52 +5,44 @@ public class AguardandoJogada implements EstadoDaPartida{
 	@Override
 	public void jogar(Partida p, Jogador jgd, Jogada j) {
 		//vou fazer algum coisa
-		
+
 		try{
 			j.baralhoValido(new ValidacaoMao());
 			p.adicionarJogada(jgd, j);
 			p.notificarObservadores(new Mensagem("jogada_realizada", jgd));
-<<<<<<< HEAD
+
 			if(p.associacaoCompleta()){
-				VerificandoPontuacao v = new VerificandoPontuacao();				
+				VerificandoPontuacao v=new VerificandoPontuacao();
 				p.fixarEstado(v);
 				v.verificandoVencedorDoTurno(p);
-				
+
 			}
-=======
-			
-                        if(p.associacaoCompleta()){
-                                VerificandoPontuacao v=new VerificandoPontuacao();
-				p.fixarEstado(v);
-                                v.verificandoVencedorDoTurno(p);
-			
-                        }
->>>>>>> 9e912f343e6a9ae557b31a1f343184799d589b8d
-			
+
+
 		}catch(ExcecaoQuebraDeRegrasDoBaralho e){
 			p.notificarObservadores(new Mensagem("Excecao", e));
 		}
-		
+
 	}
 
 	@Override
 	public void iniciar(Partida p) {
-		
-		
+
+
 	}
 
 	@Override
 	public void fimDoJogo(Partida p) {
-		
-		
+
+
 	}
 
 	@Override
 	public void verificandoVencedorDoTurno(Partida p) {
-		
-		
+
+
 	}
 
-	
+
 
 }
--- src/modelo/Partida.java
+++ src/modelo/Partida.java
@@ -7,86 +7,80 @@ import java.util.Map;
 
 public class Partida extends ObservadoImpl {
 
-    private EstadoDaPartida _estado;
-    private int _numDeJogadores;
-    private List<Jogador> jogadores = new LinkedList<Jogador>();
-    private Map<Jogador, Jogada> mapaJogadas = new Hashtable<Jogador, Jogada>();
-    private List<Observador> observadores = new LinkedList<Observador>();
-    private int _turnoAtual;
-    private ControladorDePartida _ctr;
+	private EstadoDaPartida _estado;
+	private int _numDeJogadores;
+	private List<Jogador> jogadores = new LinkedList<Jogador>();
+	private Map<Jogador, Jogada> mapaJogadas = new Hashtable<Jogador, Jogada>();
+	private List<Observador> observadores = new LinkedList<Observador>();
+	private int _turnoAtual;
+	private ControladorDePartida _ctr;
 
-<<<<<<< HEAD
-    
+	public Partida(int numDeJogadores, ControladorDePartida ctr) {
 
+		_estado = new AguardandoIniciar();//insere estado inicial
+		_numDeJogadores = numDeJogadores;
+		_turnoAtual = 0;
+		_ctr = ctr;
 
-    public Partida(int numDeJogadores) {
-=======
-    public Partida(int numDeJogadores, ControladorDePartida ctr) {
->>>>>>> 9e912f343e6a9ae557b31a1f343184799d589b8d
-        _estado = new AguardandoIniciar();//insere estado inicial
-        _numDeJogadores = numDeJogadores;
-        _turnoAtual = 0;
-        _ctr = ctr;
+	}
 
-    }
+	public void limparJogadas() {
+		mapaJogadas.clear();
+	}
 
-    public void limparJogadas() {
-        mapaJogadas.clear();
-    }
+	public void verificarJogada(Jogada j) {
 
-    public void verificarJogada(Jogada j) {
+	}
 
-    }
+	public synchronized void iniciar() {
+		_estado.iniciar(this);
+	}
 
-    public synchronized void iniciar() {
-        _estado.iniciar(this);
-    }
+	public void incrementarTurno() {
+		_turnoAtual++;
+	}
 
-    public void incrementarTurno() {
-        _turnoAtual++;
-    }
+	public boolean quantidadeJogadoresValida() {
 
-    public boolean quantidadeJogadoresValida() {
+		return jogadores.size() == _numDeJogadores;
+	}
 
-        return jogadores.size() == _numDeJogadores;
-    }
+	public boolean fimDeJogo() {
+		return _turnoAtual == 10;
+	}
 
-    public boolean fimDeJogo() {
-        return _turnoAtual == 10;
-    }
+	public void sinalizarFimDeJogo() {
+		_ctr.notificarFimDeJogo();
+	}
 
-    public void sinalizarFimDeJogo() {
-        _ctr.notificarFimDeJogo();
-    }
+	public void adicionarJogador(Jogador j) throws IllegalArgumentException {
+		if (jogadores.contains(j)) {
+			throw new IllegalArgumentException("Usuario ja inserido na partida.");
+		}
 
-    public void adicionarJogador(Jogador j) throws IllegalArgumentException {
-        if (jogadores.contains(j)) {
-            throw new IllegalArgumentException("Usuario ja inserido na partida.");
-        }
+		jogadores.add(j);
 
-        jogadores.add(j);
+	}
 
-    }
+	public void adicionarJogada(Jogador jgd, Jogada j) {
+		mapaJogadas.put(jgd, j);
+	}
 
-    public void adicionarJogada(Jogador jgd, Jogada j) {
-        mapaJogadas.put(jgd, j);
-    }
+	void fixarEstado(EstadoDaPartida e) {
+		_estado = e;
+	}
 
-    void fixarEstado(EstadoDaPartida e) {
-        _estado = e;
-    }
+	public synchronized void jogar(Jogador jgd, Jogada j) {
+		_estado.jogar(this, jgd, j);
+	}
 
-    public synchronized void jogar(Jogador jgd, Jogada j) {
-        _estado.jogar(this, jgd, j);
-    }
-
-    public boolean associacaoCompleta() {
-        for (Jogador j : jogadores) {
-            if (!(mapaJogadas.containsKey(j))) {
-                return false;
-            }
-        }
-        return true;
-    }
+	public boolean associacaoCompleta() {
+		for (Jogador j : jogadores) {
+			if (!(mapaJogadas.containsKey(j))) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
