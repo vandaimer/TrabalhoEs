f13242aab4b35cf7bc5a1cfb24fe76fe3e1c3ef6
rodrigoPOO <rodrigop_mello@hotmail.com>
2014-07-08 02:47:14 -0300

    omfg

--- src/controle/gui_jogo/EnviarJogada.java
+++ src/controle/gui_jogo/EnviarJogada.java
@@ -3,7 +3,9 @@ package controle.gui_jogo;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
+
 import modelo.jogo.Carta;
 import modelo.jogo.CartaAbstrata;
 import modelo.jogo.CartaEfeito;
@@ -26,13 +28,14 @@ public class EnviarJogada implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
-//        Random r = new Random();
-//        LinkedList<CartaAbstrata> l = new LinkedList();
-//        l.add(new Carta("larman", 1, r.nextInt(30), r.nextInt(30), r.nextInt(30)));
-//        l.add(new CartaEfeito("efeito", 2, r.nextInt(3), r.nextInt(3), r.nextInt(3)));
-//        l.add(new CartaEfeito("efeito", 3, r.nextInt(3), r.nextInt(3), r.nextInt(3)));
+        Random r = new Random();
+        LinkedList<CartaAbstrata> l = new LinkedList();
+        l.add(new Carta("larman", 1, r.nextInt(30), r.nextInt(30), r.nextInt(30)));
+        l.add(new CartaEfeito("efeito", 2, r.nextInt(3), r.nextInt(3), r.nextInt(3)));
+        l.add(new CartaEfeito("efeito", 3, r.nextInt(3), r.nextInt(3), r.nextInt(3)));
 //        _ctr.jogar(l);
-        _ctr.jogar(_jd.obterCartasAtuais());
+        List<CartaAbstrata> l1 = _jd.obterCartasAtuais();
+        _ctr.jogar(l1);
         _gui.fechar();
 
     }
